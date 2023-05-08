f74483c8294f9e872e168dbd2b72eba0133d6614
olamilly <olamyy.m@gmail.com>
2023-05-08 09:18:28 +0100

    new project

---
+++ app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp({
+  data(){
+  return{
+   fullTweet:{
+   username:"CoderInTraining", 
+   tweet:""},
+   tweets: [],
+   searchQuery:"",
+   bio:"Excited future software engineer.",
+   f: false,
+   td: false
+  }
+     },
+  methods:{
+    search1(){
+      if(this.searchQuery=="")
+        this.f=false
+      else
+        this.f=true
+    },   
+    addTweet(){
+      //save Tweet
+      this.tweets.push(
+           {
+               user: this.fullTweet.username,
+               tweet: this.fullTweet.tweet
+           }
+       );
+      this.fullTweet.tweet = "";
+      if(this.tweets.length>0){
+        this.td = true
+    }
+      if(this.tweets.length<=0)
+        this.td = false
+     },
+    deleteTweet(index){
+      this.tweets.splice(index,1)
+    }
+  },
+ computed: {
+ filteredTweets() {
+   if (this.tweets.length>0){
+     var query = this.searchQuery
+     var arr = []
+     var valObj = this.tweets.filter(function(elem){
+       if( query!="" && elem.tweet.includes(query)){
+       arr.push({
+         user: elem.user, tweet: elem.tweet
+       })
+       return elem.user
+       }
+     })
+     if (valObj.length>0){
+       return arr
+     }
+       
+     }
+ }
+ 
+ }
+})
+app.mount("#app")
